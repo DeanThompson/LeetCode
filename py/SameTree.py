b239cff5f4ee7eb3c170f8ff2507b7a9499c0289
DeanThompson <yanglianglee@gmail.com>
2014-10-09 13:56:46 +0800

    update SameTree.py

--- py/SameTree.py
+++ py/SameTree.py
@@ -18,19 +18,14 @@ class Solution:
     # @param q, a tree node
     # @return a boolean
     def isSameTree(self, p, q):     
-        if p is None and q is None:
-            return True
         if p is None or q is None:
-            return False
-        print p.val, q.val
-        if p.val != q.val:
-            return False
-        return  self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            return p == q
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
 
 if __name__ == '__main__':
     proot = TreeNode(1)
-    proot.left = TreeNode(3)
+    proot.left = TreeNode(2)
     proot.right = TreeNode(3)
 
     qroot = TreeNode(1)
